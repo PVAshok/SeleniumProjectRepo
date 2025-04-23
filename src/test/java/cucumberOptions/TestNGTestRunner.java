b8480f82aeca61c06e4cc4ec3b7a9f63234d82cd
Ashok <ashokreddypenumallu1996@gmail.com>
2025-04-23 13:00:59 +0530

    comm

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -4,12 +4,11 @@ import org.testng.annotations.DataProvider;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 
 @CucumberOptions(features="src/test/java/Features",glue= "stepDefinitions",
-monochrome=true,
+monochrome=true, 
 plugin = {"html:target/cucumber.html","json:target/cucumber,json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		}
+		"rerun:target/failed_scenarios.txt"}
 )
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	
@@ -20,5 +19,4 @@ public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	{
 		return super.scenarios();
 	}
-	
 }
