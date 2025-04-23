895d3e76248a5ca56a4f1610c49c121783c957a8
Ashok <ashokreddypenumallu1996@gmail.com>
2025-04-23 17:07:09 +0530

    added code

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/Features",glue= "stepDefinitions",
 monochrome=true, 
-plugin = {"html:target/cucumber.html","json:target/cucumber,json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+plugin = {"html:target/cucumber.html","json:target/cucumber,json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"rerun:target/failed_scenarios.txt"}
 )
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	
